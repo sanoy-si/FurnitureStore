bc6e21a7a40f9c68f731c50dad40d8bbf0618adf
Yonas Sintayehu <amyonassintayehy@gmail.com>
2023-10-17 19:17:28 +0300

    configure Swagger

--- core/serializers.py
+++ core/serializers.py
@@ -6,8 +6,10 @@ class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
                   'email', 'first_name', 'last_name']
+        ref_name = 'usercreate1'
 
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        ref_name = 'user1'
