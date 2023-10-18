3764d6778809a4336ebf1a4266be5870b64dc93d
Yonas Sintayehu <amyonassintayehy@gmail.com>
2023-10-18 09:29:53 +0300

    configure other end points in Djoser

--- Store/models.py
+++ Store/models.py
@@ -124,11 +124,23 @@ class CustomOrder(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField(null=True, blank=True)
 
+
+    def __str__(self):
+        return self.product_name
+    
+
 class WishList(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.customer.user.username
+    
+
 class WishListItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     wishlist = models.ForeignKey(WishList,on_delete=models.CASCADE, related_name='wishlistitems')
+
+    def __str__(self):
+        return self.product.title
 
